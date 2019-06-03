f05c0f9b1e14121245b1e821a24653208dcf86ae
rafaelrv95 <rafaelrodriguez1995@gmail.com>
2019-06-03 15:17:49 -0500

    registrar usuario campos completos

--- usuarios/views.py
+++ usuarios/views.py
@@ -8,9 +8,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 
+from usuarios.forms import RegistroForm
 
 class RegistroUsuario(CreateView):
     model = User
     template_name = 'usuarios/registrar.html'
-    form_class = UserCreationForm
+    form_class = RegistroForm
     success_url = reverse_lazy('album:album_listar')
