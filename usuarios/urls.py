8e73210f5e14ef9625dd503d304df6019ff694d8
rafaelrv95 <rafaelrodriguez1995@gmail.com>
2019-06-06 19:55:10 -0500

    funcion de crear usuario solo para administrador

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 from usuarios.views import RegistroUsuario
 
 urlpatterns = [
-    url(r'^registrar',login_required(RegistroUsuario.as_view()), name='registrar'),
+    url(r'^registrar',staff_member_required(login_required(RegistroUsuario.as_view())), name='registrar'),
     
 ]
